33e76a55ffbb88cb5de445d62aef9fff3e5e1c2c
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-09-04 10:19:52 -0500

    Retrieve all cell info for each sheet

--- ReadExcel.py
+++ ReadExcel.py
@@ -1,10 +1,9 @@
 from openpyxl import load_workbook
 wb = load_workbook(filename = 'xlsx/BMW_Sales_Standards_2016_ME.xlsx', data_only=True)
 
-ws = wb['Section 1_Brand Architecture']
-
-cell_range = ws['Z6':'Z18']
-
-for i, rowOfCellObjects in enumerate(cell_range):
-  for n, cellObj in enumerate(rowOfCellObjects):
-    print cellObj.value
+for sheet in wb.sheetnames:
+  ws = wb[sheet]
+  cell_range = ws['Z6':'Z18']
+  for i, rowOfCellObjects in enumerate(cell_range):
+    for n, cellObj in enumerate(rowOfCellObjects):
+      print cellObj.value

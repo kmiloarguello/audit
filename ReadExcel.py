fb9c1adb924cc0b73f7dcd1aaf992907bbbac008
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-09-05 17:33:45 -0500

    Got across array when is not None

--- ReadExcel.py
+++ ReadExcel.py
@@ -1,9 +1,20 @@
 from openpyxl import load_workbook
+from openpyxl.worksheet import Worksheet
+
 wb = load_workbook(filename = 'xlsx/BMW_Sales_Standards_2016_ME.xlsx', data_only=True)
 
-for sheet in wb.sheetnames:
+sheets = wb.sheetnames[3:13]
+count = 100
+sizeCell = []
+
+
+
+for sheet in sheets:
   ws = wb[sheet]
-  cell_range = ws['Z6':'Z18']
+  cell_range = ws['X6':'X200']
+  data = ws['X6':'X200']
   for i, rowOfCellObjects in enumerate(cell_range):
     for n, cellObj in enumerate(rowOfCellObjects):
-      print cellObj.value
+      if cellObj.value is not None:
+        sizeCell.append(cellObj.value)
+        print sizeCell

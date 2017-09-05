35a81ea6798b0956d6ee981dd5d959e09d05ed5a
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-09-05 18:55:45 -0500

    Retrieve all row data for specific cell

--- ReadExcel.py
+++ ReadExcel.py
@@ -1,20 +1,10 @@
 from openpyxl import load_workbook
-from openpyxl.worksheet import Worksheet
 
-wb = load_workbook(filename = 'xlsx/BMW_Sales_Standards_2016_ME.xlsx', data_only=True)
-
-sheets = wb.sheetnames[3:13]
-count = 100
-sizeCell = []
-
-
+wb = load_workbook(filename = 'xlsx/libro.xlsx', data_only=True)
+sheets = wb.sheetnames[0:1]
 
 for sheet in sheets:
   ws = wb[sheet]
-  cell_range = ws['X6':'X200']
-  data = ws['X6':'X200']
-  for i, rowOfCellObjects in enumerate(cell_range):
-    for n, cellObj in enumerate(rowOfCellObjects):
-      if cellObj.value is not None:
-        sizeCell.append(cellObj.value)
-        print sizeCell
+  cell = ws['B2']
+  for column in ws.columns:
+    print column[1].value

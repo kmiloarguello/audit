dcb8e037dc3d0699248df020b474607d03d8fbb2
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-09-06 08:38:39 -0500

    Get all row data for cll B6 sheet Section9 of excel file

--- ReadExcel.py
+++ ReadExcel.py
@@ -1,10 +1,11 @@
 from openpyxl import load_workbook
 
-wb = load_workbook(filename = 'xlsx/libro.xlsx', data_only=True)
-sheets = wb.sheetnames[0:1]
+wb = load_workbook(filename = 'xlsx/BMW_Sales_Standards_2016_ME.xlsx', data_only=True)
+sheets = wb.sheetnames[11:12]
 
 for sheet in sheets:
+  print sheet
   ws = wb[sheet]
-  cell = ws['B2']
+  cell = ws['B6']
   for column in ws.columns:
-    print column[1].value
+    print column[5].value

ffbab54af2ca0a39e9904f3de739e0a0afc49983
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-09-21 11:06:39 -0500

    Creating code for all sheets

--- ReadExcel.py
+++ ReadExcel.py
@@ -3,7 +3,7 @@ from openpyxl import Workbook
 from openpyxl.writer.write_only import WriteOnlyCell
 
 wb = load_workbook(filename = 'xlsx/BMW_Sales_Standards_2016_ME.xlsx', data_only=True)
-sheets = wb.sheetnames[11:12] #Current Sheet
+sheets = wb.sheetnames[10:12] #Current Sheet
 
 # Arrays
 numberCategory = []
@@ -13,12 +13,12 @@ cleanedRowData = []
 
 for sheet in sheets: #In current sheet give me the rows and columns
   ws = wb[sheet] # Pass the info as ws variable
-  
+
   for row in ws.rows: #Get all rows
     numberCategory.insert(0,row[23].value)  #Get the category number and insert the value of row #1 in excel => B
     number_categories_without_filter = next(i for i in numberCategory if i is not None) #Clean of every None and replacing for the previous valid element
     index_number_categories.extend([number_categories_without_filter]) # Store data row into a Array to see its index
-    
+
     if(row[23].value == "N"): #If X1 to Xn something has N get that result
       n_result = [] # Empty array to add values with N
       
@@ -26,16 +26,17 @@ for sheet in sheets: #In current sheet give me the rows and columns
         
         if index_number_categories[i] == 'N': # set the i into the array if are equal to "N"
           n_result.append(i) # add this index to n_result the N values
-      
+
       negatives = max(n_result) #save the max result of array i.e. of a len of 3 items should be => [a,b,c] instead => [a] or => [a,b]
-
+    
       for column in ws.columns: #Get info of current row that has N in that column
-        rowData.insert(0,column[negatives].value)  #Get the category number and insert the value of row #1 in excel => B
+        print column[0]
+        # rowData.append([column[negatives]])  #Get the category number and insert the value of row #1 in excel => B
 
-        wbp = Workbook(write_only=True) #Call again Workbook with parameter of writing new file
-        wsp = wbp.create_sheet() #Create sheet
-        for i in range(len(rowData)): # In rowData get all the data
-          wsp.append([rowData[i]]) # append it into a cells
+  #       wbp = Workbook(write_only=True) #Call again Workbook with parameter of writing new file
+  #       wsp = wbp.create_sheet() #Create sheet
+  #       for i in range(len(rowData)): # In rowData get all the data
+  #         wsp.append([rowData[i]]) # append it into a cells
 
-        wbp.save('result.xlsx') # Save excel file
+  #       wbp.save('result.xlsx') # Save excel file
 
--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print "Holaaa Camilo"
+from openpyxl import Workbook
+wb = Workbook()
+
+ws = wb.active
+
+ws['A1'] = 42
+ws['A2'] = 88
+ws.append([1, 2, 3])
+
+# Save the file
+wb.save("sample.xlsx")

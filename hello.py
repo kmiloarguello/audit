331e883f4d2d7be2593171c44744ff3fd7862d33
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-09-25 13:32:40 -0500

    changing askopenfile

--- hello.py
+++ hello.py
@@ -1,11 +1,37 @@
-from openpyxl import Workbook
-wb = Workbook()
+import sys
+from Tkinter import *
+from tkFileDialog import askopenfilename
 
-ws = wb.active
+class App(Frame):
+  def __init__(self, master=None):
+    Frame.__init__(self,master)
+    self.pack()
+    self.createWidgets()
 
-ws['A1'] = 42
-ws['A2'] = 88
-ws.append([1, 2, 3])
+  def createWidgets(self):
+    self.QUIT = Button(self)
+    self.QUIT['text'] = 'Salir'
+    self.QUIT['fg'] = 'red'
+    self.QUIT['command'] = self.quit
+    self.QUIT.pack(side=LEFT, padx=10,pady=10)
+  
+window = Tk()
+window.title('K@PTA Excel Auditorias')
+window.wm_iconbitmap('img/kapta_mex.ico')
+window.geometry('{}x{}'.format(800, 600))
+window.resizable(0,0)
 
-# Save the file
-wb.save("sample.xlsx")
+window.fileName = filedialog.askopenfilename( filetypes = (("howCode files", ".hc"), ("All files", "*.*")))
+
+
+# window.configure(background="#ffffff")
+
+# bg = PhotoImage(file="img/bg.gif")
+# img = Label(window, image=bg)
+# img.place(x=0,y=0, relwidth=1,relheight=1)
+# img.pack()
+
+app = App(master=window)
+
+app.mainloop()
+window.destroy()

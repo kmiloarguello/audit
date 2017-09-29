a324bd4f994282f4de9ec37f5fa1bbff6ec4c819
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-09-29 15:19:27 -0500

    Finishing iteration

--- hello.py
+++ hello.py
@@ -85,15 +85,33 @@ rowData = []
 cleanedRowData = []
 auditvalue = []
 essential = []
+standard = []
+number = []
+requirement = []
+comments = []
+question = []
+observation = []
+suggested = []
 
 wb = load_workbook(filename = window.filename, data_only=True)
 sheets = wb.sheetnames[3:12]
 
 myHoja = []
-myN = []
-myZero = []
+myStandard = []
+myNumber = []
+myRequeriment = []
+myComment = []
 myAudit = []
 myEssentials = []
+myAuditQuestion = []
+myObservation = []
+mySuggested = []
+myN = []
+myZero = []
+myAComments = []
+myPic = []
+auditcomments = []
+picture = []
 
 for sheet in sheets:
   ws = wb[sheet]
@@ -112,12 +130,51 @@ for sheet in sheets:
     essential.insert(0,row[15].value)  
     essential_without_filter = next(i for i in essential if i is not None)
 
+    standard.insert(0,row[0].value)  
+    standard_categories_without_filter = next(i for i in standard if i is not None)
+
+    number.insert(0,row[1].value)  
+    number_categories_without_filter = next(i for i in number if i is not None)
+
+    requirement.insert(0,row[2].value)  
+    requirement_categories_without_filter = next(i for i in requirement if i is not None)
+
+    comments.insert(0,row[4].value)  
+    comments_categories_without_filter = next(i for i in comments if i is not None)
+
+    question.insert(0,row[17].value)  
+    question_categories_without_filter = next(i for i in question if i is not None)
+
+    observation.insert(0,row[19].value)  
+    observation_categories_without_filter = next(i for i in observation if i is not None)
+
+    suggested.insert(0,row[21].value)  
+    suggested_categories_without_filter = next(i for i in suggested if i is not None)
+
+    auditcomments.insert(0,row[30].value)  
+    auditcomments_categories_without_filter = next(i for i in auditcomments if i is not None)
+
+    picture.insert(0,row[30].value)  
+    picture_categories_without_filter = next(i for i in picture if i is not None)
+
+
     if(row[23].value == "N" and zero_categories_without_filter == 0):
       myHoja.extend([sheet])
       myN.extend([str(row[23].value)])
       myZero.extend([str(zero_categories_without_filter)])
       myAudit.extend([str(audit_categories_without_filter)])
       myEssentials.extend([str(essential_without_filter)])
+      
+      myStandard.extend([str(standard_categories_without_filter)])
+      myNumber.extend([str(number_categories_without_filter)])
+      myRequeriment.extend([requirement_categories_without_filter.encode('utf-8')])
+      myComment.extend([str(comments_categories_without_filter)])
+      myAuditQuestion.extend([str(question_categories_without_filter)])
+      myObservation.extend([str(observation_categories_without_filter)])
+      mySuggested.extend([str(suggested_categories_without_filter)])
+      myAComments.extend([str(auditcomments_categories_without_filter)])
+      myPic.extend([str(picture_categories_without_filter)])
+
 
 tframe = Frame(window)
 tframe.pack()
@@ -148,21 +205,22 @@ table.addColumn('Result')
 table.addColumn('Audit Comments')
 table.addColumn('Picture / Statement / Proof')
 
+
 for i in range(len(myHoja)):
   table.model.data[i]['Hoja Excel'] = myHoja[i]
-  # table.model.data[i]['Standard'] = 0
-  # table.model.data[i]['Number'] 
-  # table.model.data[i]['Requirement 2015']
-  # table.model.data[i]['Comments']
+  table.model.data[i]['Standard'] = myStandard[i]
+  table.model.data[i]['Number'] = myNumber[i]
+  table.model.data[i]['Requirement 2015'] = myRequeriment[i]
+  table.model.data[i]['Comments'] = myComment[i]
   table.model.data[i]['Type of Check'] = myAudit[i]
   table.model.data[i]['Essentials'] = myEssentials[i]
-  # table.model.data[i]['Audit Question']
-  # table.model.data[i]['Observation / Evidence Required / Audit Remarks']
-  # table.model.data[i]['Suggested Person to ask']
+  table.model.data[i]['Audit Question'] = myAuditQuestion[i]
+  table.model.data[i]['Observation / Evidence Required / Audit Remarks'] = myObservation[i]
+  table.model.data[i]['Suggested Person to ask'] = mySuggested[i]
   table.model.data[i]['Evaluation (0/1)'] = myN[i]
   table.model.data[i]['Result'] = myZero[i]
-  # table.model.data[i]['Audit Comments']
-  # table.model.data[i]['Picture / Statement / Proof']
+  table.model.data[i]['Audit Comments'] = myAComments[i]
+  table.model.data[i]['Picture / Statement / Proof'] = myPic[i]
 
 
 table.redrawTable()

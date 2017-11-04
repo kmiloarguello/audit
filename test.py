e526407cd9c42e4200283fc5a5072e77f6ff151a
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-11-04 15:37:52 -0500

    On blank cell store the path of file

--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-"""
-Proyecto desarrollado por KAPTA SAS Colombia.
-Las librerias usadas son de uso general. 
-El proposito de este codigo y/o proyecto es para uso interno de la empresa.
-
-Desarrollado por CAMILO ARGUELLO https://camiloarguello.xyz
-
-Derechos reservados para KAPTA SAS. http://kapta.biz
-Colombia
-2017
-
-"""
-
 from Tkinter import *
 from PIL import Image, ImageTk
 from tkFileDialog import askopenfilename, asksaveasfilename
@@ -19,7 +6,6 @@ from openpyxl import load_workbook
 from openpyxl import Workbook
 from openpyxl.writer.write_only import WriteOnlyCell
 import types
-import os
 # from pandastable import Table, TableModel
 from tkintertable.Tables import TableCanvas
 from tkintertable.TableModels import TableModel
@@ -38,8 +24,8 @@ class OtherFrame(Toplevel):
       
       #Center the window
 
-      myW = 1200
-      myH = 600
+      myW = 800
+      myH = 400
 
       myWs = root.winfo_screenwidth()
       myHs = root.winfo_screenheight()
@@ -67,19 +53,28 @@ class OtherFrame(Toplevel):
       self.frame = Frame(self)
       self.loadExcel()
       self.renderExcel()
-      self.frame.pack(fill=X)
+      self.frame.pack(fill=BOTH)
 
       """
       Frame to upload the image and manage it.
       """
 
       self.line = Frame(self, relief=RAISED, borderwidth=1)
-      self.line.pack(fill=X,)
+      self.line.pack(fill=BOTH, expand=True)
 
-      self.image_container = Frame(self)
-      Label(self.image_container, text='Imagenes para guardar en excel', font=("Helvetica", 15), foreground='#000').pack(pady=20)
-      self.buttonImage()
-      self.image_container.pack(fill=X, pady=5)
+      # self.image_container = Frame(self)
+      # Label(self.image_container, text='Imagenes para guardar en excel', font=("Helvetica", 15), foreground='#000').pack(pady=20)
+      # self.buttonImage()
+      # self.image_container.pack(fill=X, pady=5)
+
+      """
+      Button of CLOSE
+      """
+
+      self.frame2 = Frame(self)
+      btn = Button(self.frame2, text="Close", command=self.onClose)
+      btn.pack()
+      self.frame2.pack()
 
       """
       # Scrollbar funcionality
@@ -95,8 +90,6 @@ class OtherFrame(Toplevel):
       self.scrollbar.config(command=self.listbox.yview)
 
       """
-
-      self.protocol('WM_DELETE_WINDOW', self.onClose)
 
       self.footer()
 
@@ -165,16 +158,9 @@ class OtherFrame(Toplevel):
       self.picture = []        
     
     def loadExcel(self):
-      try:
-        self.filename = askopenfilename( title = "Subir archivo de excel", filetypes = (("Excel Auditorias", ".xlsx"), ("Todos los archivos", "*.*")))  
-        self.arraysInit()
-        self.loadWorkbook()
-      except:
-        tkMessageBox.showwarning(
-            "Error al subir archivo",
-            "Solo puedes cargar archivos de auditorias.\n(%s)" % self.filename
-        )
-        return (root.quit())
+      self.filename = askopenfilename( initialdir = "/KAPTA Camilo/python/xlsx",title = "Subir archivo de excel", filetypes = (("Excel Auditorias", ".xlsx"), ("Todos los archivos", "*.*")))  
+      self.arraysInit()
+      self.loadWorkbook()
 
     def withoutFilter(self,item,cell):
       """
@@ -195,13 +181,7 @@ class OtherFrame(Toplevel):
        First Load the excel file
       """
       self.wb = load_workbook(filename=self.filename,data_only=True)
-      isAuditExcel = []
-      for s in self.wb:
-        isAuditExcel.extend([s])
-        if len(isAuditExcel) >= 12:
-          self.sheets = self.wb.sheetnames[3:12]
-        else:
-          print ''
+      self.sheets = self.wb.sheetnames[3:12]
 
       for sheet in self.sheets:
         self.ws = self.wb[sheet]
@@ -217,18 +197,14 @@ class OtherFrame(Toplevel):
           final_question = self.withoutFilter(self.question,row[17])
           final_observation = self.withoutFilter(self.observation,row[19])
           final_suggested = self.withoutFilter(self.suggested,row[21])
+          final_auditcomments = self.withoutFilter(self.auditcomments,row[30])
+          final_picture = self.withoutFilter(self.picture,row[30])
 
-          if len(row) == 31:
-            final_auditcomments = self.withoutFilter(self.auditcomments,row[28])
-            final_picture = self.withoutFilter(self.picture,row[30])
-          else:
-            final_auditcomments = self.withoutFilter(self.auditcomments,row[28])
-            final_picture = self.withoutFilter(self.picture,row[30])
           
           if(row[23].value == "N" and final_zero == 0 and 'Audit' in final_audit ):
             self.myHoja.extend([sheet])
-            self.myN.extend([str(row[23].value)])
-            self.myZero.extend([str(final_zero)])
+            self.myN.extend([row[23]])
+            self.myZero.extend([final_zero])
             self.myAudit.extend([final_audit])
             self.myEssentials.extend([final_essential])
             self.myStandard.extend([final_standard])
@@ -243,7 +219,7 @@ class OtherFrame(Toplevel):
         
     def renderExcel(self):
       self.model = TableModel()
-      self.table = TableCanvas(self.frame,model=self.model,editable=False,rowheaderwidth=50)
+      self.table = TableCanvas(self.frame,model=self.model,editable=False,rowheaderwidth=100)
       self.table.createTableFrame()
       self.model = self.table.model
 
@@ -257,7 +233,7 @@ class OtherFrame(Toplevel):
       self.table.addColumn('Hoja Excel')
       self.table.addColumn('Standard')
       self.table.addColumn('Number')
-      self.table.addColumn('Requirement')
+      self.table.addColumn('Requirement 2015')
       self.table.addColumn('Comments')
       self.table.addColumn('Type of Check')
       self.table.addColumn('Essentials')
@@ -267,13 +243,13 @@ class OtherFrame(Toplevel):
       self.table.addColumn('Evaluation (0/1)')
       self.table.addColumn('Result')
       self.table.addColumn('Audit Comments')
-      # self.table.addColumn('Picture / Statement / Proof')
+      self.table.addColumn('Picture / Statement / Proof')
 
       for i in range(len(self.myHoja)):
         self.table.model.data[i]['Hoja Excel'] = self.myHoja[i]
         self.table.model.data[i]['Standard'] = self.myStandard[i]
         self.table.model.data[i]['Number'] = self.myNumber[i]
-        self.table.model.data[i]['Requirement'] = self.myRequeriment[i]
+        self.table.model.data[i]['Requirement 2015'] = self.myRequeriment[i]
         self.table.model.data[i]['Comments'] = self.myComment[i]
         self.table.model.data[i]['Type of Check'] = self.myAudit[i]
         self.table.model.data[i]['Essentials'] = self.myEssentials[i]
@@ -283,20 +259,19 @@ class OtherFrame(Toplevel):
         self.table.model.data[i]['Evaluation (0/1)'] = self.myN[i]
         self.table.model.data[i]['Result'] = self.myZero[i]
         self.table.model.data[i]['Audit Comments'] = self.myAComments[i]
-        # self.table.model.data[i]['Picture / Statement / Proof'] = self.myPic[i]
+        self.table.model.data[i]['Picture / Statement / Proof'] = self.myPic[i]
 
       self.table.redrawTable()
     
     #----------------------------------------------------------------------
     """
-    LOAD IMAGE Funcionality
+    LOAD EXCEL Funcionality
     
-    1. buttonImage() -> Shows the button to upload the image
-    2. loadImage() -> Load the image file
-    3. contImage() -> Elements to Entry values and select Sheet when it selected call the next method
-    4. Sheet_select() -> Select the Item and call the next function
-    5. item_selected() -> Select the requirement of that year. and call next function
-    6. saveImageToExcel() -> When the item is selected it save into a excel file.
+    1. arraysInit() -> is the initialization of every array
+    2. loadExcel() -> Load the excel file
+    3. withoutFilter() -> Return a list with cell without None values
+    4. loadWorkbook() -> Load from the file, the sheets and each column. Make search functionality
+    5. renderExcel() -> Puth the info of file inside of a canvas using tkintertables
 
     """
     def buttonImage(self):
@@ -306,9 +281,8 @@ class OtherFrame(Toplevel):
       self.load_cont_img.pack(side=TOP,fill=X)
     
     def loadImage(self):
-      self.file_image = askopenfilename(title = "Subir imagen para guardar en excel", filetypes = (("Imagen de resultado", ".jpg"), ("Todos los archivos", "*.*")))
-      if self.file_image is not None:
-        self.contImage()
+      self.file_image = askopenfilename( initialdir = "/KAPTA Camilo/python/img",title = "Subir imagen para guardar en excel", filetypes = (("Imagen de resultado", ".jpg"), ("Todos los archivos", "*.*")))
+      self.contImage()
     
     def contImage(self):
       self.rutaImg = Frame(self.image_container)
@@ -319,110 +293,37 @@ class OtherFrame(Toplevel):
       Label(self.rutaImg, text=self.file_image, font=("Helvetica", 8), foreground='#000').grid(row=2, column=2)
 
       Label(self.rutaImg, text='Nombre a guardar', font=("Helvetica", 10), foreground='#E38929').grid(row=1, column=4)
-      self.image_path = StringVar()
-      entry = Entry(self.rutaImg,textvariable=self.image_path)
+      entry = Entry(self.rutaImg,textvariable="entry_txt")
       entry.grid(row=2, column=4)
 
-      Label(self.rutaImg, text='Selecciona Hoja', font=("Helvetica", 10), foreground='#E38929').grid(row=1, column=8)
-      ex_sh_sel = StringVar(self.rutaImg)
-      ex_sh_sel.set(self.sheets[0])
-      w = OptionMenu(self.rutaImg, ex_sh_sel, *self.sheets,command=self.sheet_selected)
-      w.grid(row=2,column=8)
+      Button(self.rutaImg, text="Opciones", command=self.optionsImages()).grid(row=2, column=6)
 
       self.rutaImg.pack(side=TOP, fill=X)
+    
+    def optionsImages(self):
+      self.optionImage = Toplevel(self)
+      self.optionImage.title('K@PTA')
+      self.optionImage.wm_iconbitmap('kapta_mex.ico')
+      self.optionImage.geometry('400x100')
 
-    def sheet_selected(self,value):
-      '''
-      Here I catch the sheets. 
-      1. as I have the self.sheets array
-      2. using a for I looped it to find when the value passed is equal to i
-      3. I assign a and b knowing that the sheets start on number 3 sheet to number 12
-      That give me the info of search
-      4. Loop into sheet filtered
-      5. Catch info of that sheet and store into a my_ws
-      6. Loop it and find the row of the data of that sheet
-      If is None and Number skip.
-      7. Call an array and add the info of values
-      Assign it into a OptionMenu of Tkinter.
+      # Label(self.optionImage, text="  ").grid(row=1, column=0)
+      # Label(self.optionImage, text="  ").grid(row=2, column=0)
 
-      '''
-      item_searched = []
+      # Label(self.optionImage, text='Selecciona el archivo para guardar', font=("Helvetica", 10)).grid(row=1, column=2)
 
-      self.final_sheet_render = value
+      # # Button(self.optionImage, text="Guardar en Excel", command=loadExceltoSave).grid(row=2, column=2)
 
-      Label(self.rutaImg, text='Seleccione Item', font=("Helvetica", 10), foreground='#E38929').grid(row=1, column=10)
+      # Label(self.optionImage, text='Selecciona Hoja', font=("Helvetica", 10), foreground='#E38929').grid(row=3, column=2)
+      # ex_sh_sel = StringVar(self.optionImage)
+      # ex_sh_sel.set('Section 1_Brand Architecture')
+      # option = OptionMenu(self.optionImage, ex_sh_sel, 'Section 2_OCS', 'Section 4_Customer Area', 'Section 5_IT', 'Section 6_Management','Section 7_Personnel  Training','Section 8_Customer Processes','Section 9_Marketing').grid(row=4, column=2)
 
-      for i in range(len(self.sheets)):
-        if value == self.sheets[i]: 
-          a = i + 3
-          b = a + 1
-          self.sheets_search = self.wb.sheetnames[a:b]
+      # Label(self.optionImage, text='Seleccione Item', font=("Helvetica", 10), foreground='#E38929').grid(row=3, column=5)
+      # ex_sh_sel2 = StringVar(self.optionImage)
+      # ex_sh_sel2.set('0.0')
+      # option2 = OptionMenu(self.optionImage, ex_sh_sel2, '1.0', '2.0', '3.0', '4.0').grid(row=4, column=5)
 
-      for my_sheet in self.sheets_search:
-        my_ws = self.wb[my_sheet]
-        for my_row in my_ws.rows:
-          if my_row[1].value is not None:
-            final_number_tkinter = my_row[1].value
-            if not 'Number' in final_number_tkinter:
-              item_searched.extend([final_number_tkinter])
-              ex_it_sel = StringVar(self.rutaImg)
-              ex_it_sel.set(item_searched[0])
-              item = OptionMenu(self.rutaImg, ex_it_sel, *item_searched, command=self.item_selected)
-              item.grid(row=2,column=10)
-
-      return value
-
-    def item_selected(self,value):
-      req_searched = []
-      Label(self.rutaImg, text='Seleccione Requerimiento', font=("Helvetica", 10), foreground='#E38929').grid(row=1, column=12)
-
-      self.item_value = value
-
-      for my_sheet in self.sheets_search:
-        my_ws = self.wb[my_sheet]
-        for my_row in my_ws.rows:
-          if(value == my_row[1].value):
-            if my_row[2].value is not None:
-              max_letter = my_row[2].value.encode('ascii', 'ignore')[:10]
-              req_searched.extend([max_letter])
-              ex_rq_sel = StringVar(self.rutaImg)
-              ex_rq_sel.set(req_searched[0])
-              req = OptionMenu(self.rutaImg, ex_rq_sel, *req_searched, command=self.saveImageToExcel)
-              req.grid(row=2,column=12)
-      return value
-
-    def saveImageToExcel(self,value):
-      """
-      Loop inside the excel filtered to store the data.
-      self.item.value is the value of the item selected
-      valor_a_guardar is the value of Entry text box. It passed as STR using encode utf-8
-
-      Store Data
-      1. Get the sheet by name
-      2. Get the cell value and coodinates
-      3. Save the new excel or replace the last version
-
-      Call the label to show to the user that the image was stored on excel
-      """
-
-      for my_sheet in self.sheets_search:
-        my_ws = self.wb[my_sheet]
-        for my_row in my_ws.rows:
-          if self.item_value == my_row[1].value:
-            if my_row[2].value is not None:
-              valor_a_guardar = self.image_path.get().encode('utf-8')
-              f_sheet = self.wb.get_sheet_by_name(my_sheet)
-              f_celda = my_row[len(my_row)-1].coordinate
-
-              if valor_a_guardar == "":
-                get_only_file = os.path.split(self.file_image)[1]
-                valor_a_guardar = get_only_file
-
-              f_sheet[f_celda] = valor_a_guardar
-
-              self.wb.save(self.filename)
-      
-      Label(self.rutaImg, text='Imagen guardada en excel', font=("Helvetica", 10), foreground='#00FF00').grid(row=2, column=14)
+      # Button(self.optionImage, text="Guardar en Excel", command=saveFile).grid(row=4, column=7)
     
     #----------------------------------------------------------------------
     def footer(self):
@@ -438,7 +339,6 @@ class OtherFrame(Toplevel):
       self.about.wm_iconbitmap('kapta_mex.ico')
       self.about.geometry('200x100')
       Label(self.about, text='Derechos Reservados K@PTA \n Desarrollador Camilo Arguello \n Farrell, D 2016 DataExplore: An Application for General Data Analysis in Research and Education. Journal of Open Research Software, 4: e9, DOI: http://dx.doi.org/10.5334/jors.94', font=("Segoe UI", 9), justify=LEFT).pack()
-    
     #----------------------------------------------------------------------
     def onClose(self):
       
@@ -477,7 +377,7 @@ class MyApp(object):
 
         self.label = Label(self.frame, text='Para iniciar por favor carga el archivo de auditorias')
         self.button = Button(self.frame, justify=LEFT,command=self.openFrame)
-        self.photo = ImageTk.PhotoImage(file='subir_excel.png')
+        self.photo = ImageTk.PhotoImage(file='img/subir_excel.png')
         self.button.config(image=self.photo, width='300',height='50')
         self.label.pack(fill=X)
         self.button.pack()
